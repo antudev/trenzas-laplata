ccca89d31de1a449f126727a512b07f1fe5abc7c
Antu <antu.almonacid@alu.ing.unlp.edu.ar>
2023-10-18 08:44:50 -0300

    Netlify.toml file fix

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///catalog.db'
+# Inicializar la extensión SQLAlchemy
 db = SQLAlchemy(app)
 
 # Configura la carpeta de plantillas (templates)
